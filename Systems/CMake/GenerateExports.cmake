224e21772d96c2ed2876572bc65ec0a257a05500
Mario Garcia <alexander.garcia18@yahoo.com>
2026-07-31 19:44:46 -0700

    CMake update dll for tests through cmake. Need to make a build system for the base framework.

--- Systems/CMake/GenerateExports.cmake
+++ Systems/CMake/GenerateExports.cmake
@@ -1,8 +1,8 @@
 # Cmake generation for auto exports and imports
 
 # Internal function to call for ExportThisLibrary.
-function(ExportLibraryInternal TARGET_LIBRARY)
-	set(RECLUSE_GENERATED_LIBRARY_DIRECTORY ${CMAKE_SOURCE_DIR}/Recluse/Build/Libraries)
+function(ExportLibraryInternal TARGET_LIBRARY BUILD_PATH)
+	set(RECLUSE_GENERATED_LIBRARY_DIRECTORY ${BUILD_PATH})
 	set(EXPORT_LIBRARY_DIRECTORY ${RECLUSE_GENERATED_LIBRARY_DIRECTORY}/${TARGET_LIBRARY})
 	set(EXPORT_LIBRARY_FILE ${EXPORT_LIBRARY_DIRECTORY}/${TARGET_LIBRARY}_exports.hpp)
 	set(RECLUSE_EXPORT_NAME ${TARGET_LIBRARY})
@@ -17,16 +17,16 @@ endfunction()
 
 
 # ExportThisLibrary is intended to be used for projects that want to export their api to other projects/libraries/applications.
-function(ExportThisLibrary TARGET_LIBRARY)
-	ExportLibraryInternal(${TARGET_LIBRARY})
+function(ExportThisLibrary TARGET_LIBRARY BUILD_PATH)
+	ExportLibraryInternal(${TARGET_LIBRARY} ${BUILD_PATH})
 	#add_custom_command(TARGET ${TARGET_LIBRARY} PRE_BUILD COMMAND ExportLibraryInternal ARGS ${TARGET_LIBRARY})
 endfunction()
 
 
 # ImportThisLibrary is intended to be used for projects that want to import an api from another library/project to the current 
 # target library that they are building/using.
-function(ImportThisLibrary TARGET_LIBRARY IMPORT_LIBRARY)
-	set(RECLUSE_GENERATED_LIBRARY_DIRECTORY ${CMAKE_SOURCE_DIR}/Recluse/Build/Libraries)
+function(ImportThisLibrary TARGET_LIBRARY IMPORT_LIBRARY BUILD_PATH)
+	set(RECLUSE_GENERATED_LIBRARY_DIRECTORY ${BUILD_PATH})
 	set(IMPORT_LIBRARY_DIRECTORY ${RECLUSE_GENERATED_LIBRARY_DIRECTORY}/${IMPORT_LIBRARY})
 	target_include_directories(${TARGET_LIBRARY} PUBLIC ${IMPORT_LIBRARY_DIRECTORY})
 endfunction()
